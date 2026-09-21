16f5fe685b3509580dce7c61337a0bcfc55cec54
Santhanamariammal24 <santhanamariammalrajamani1998@gmail.com>
2026-09-21 23:18:36 +0530

    Practice JavaScript variables

---
+++ 02-variable.js
@@ -0,0 +1,8 @@
+let name = "Santhanamariammal";
+let experience = 4;
+let CurrentRole = "Associate,Quality Services";
+let goal = "Will get a high paying automation job";
+console.log(name);
+console.log(experience);
+console.log(CurrentRole);
+console.log(goal);

97ef6ee7246ef248b20c9d32f2859b2743028714
Santhanamariammal24 <santhanamariammalrajamani1998@gmail.com>
2026-09-21 23:36:48 +0530

    Variable Challenge

---
+++ 05-ChallengeVariable.js
@@ -0,0 +1,8 @@
+let browser="Chrome";
+let website="Amazon";
+let testcase="Login Test";
+let testStatus="Passed";
+console.log(browser);
+console.log(website);
+console.log(testcase);
+console.log(testStatus);

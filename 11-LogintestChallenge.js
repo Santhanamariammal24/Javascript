b7ff68c2067a6e9098a66fa626855a3e6b0f71d5
Santhanamariammal24 <santhanamariammalrajamani1998@gmail.com>
2026-09-22 12:05:11 +0530

    Login test challenge

---
+++ 11-LogintestChallenge.js
@@ -0,0 +1,8 @@
+let username ="admin";
+let password ="admin123";
+let accountLocked =false;
+if(username === "admin" && password ==="admin123" && !accountLocked){
+    console.log("Login Successful");
+}else{
+    console.log("Login Failed");
+}

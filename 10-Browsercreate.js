2eaf3df91a79ea15df0000bdf7c4459bef8a1bdc
Santhanamariammal24 <santhanamariammalrajamani1998@gmail.com>
2026-09-22 12:04:06 +0530

    test runs in Different Browser

---
+++ 10-Browsercreate.js
@@ -0,0 +1,9 @@
+let browser ="chrome";
+if(browser === "chrome"){
+    console.log("Running tests in chrome");
+}else if(browser ==="firefox"){
+    console.log("Running tests in firefox");
+}else if(browser ==="anything else"){
+    console.log("browser not supported");
+}
+

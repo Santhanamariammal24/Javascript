8c54f29d821020e8bc1679e724088b2032caed2e
Santhanamariammal24 <santhanamariammalrajamani1998@gmail.com>
2026-09-22 23:08:53 +0530

    Function with return

---
+++ 16-Return.js
@@ -0,0 +1,5 @@
+function add(a, b) {
+    return a + b; // This is a simple example of using return statement in JavaScript to return the sum of two numbers.
+}
+
+console.log(add(10, 20));// This is a simple example of using return statement in JavaScript to return the sum of two numbers.

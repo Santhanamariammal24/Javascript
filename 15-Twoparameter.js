e5d858bbc659cb06636984b8c1014376f4e13636
Santhanamariammal24 <santhanamariammalrajamani1998@gmail.com>
2026-09-22 23:06:43 +0530

    Function program with two parameter

---
+++ 15-Twoparameter.js
@@ -0,0 +1,5 @@
+function login(username, password) { //function with two parameters
+    console.log("Username: " + username);
+    console.log("Password: " + password);
+}
+login("admin", "admin123"); //calling the function with two parameters

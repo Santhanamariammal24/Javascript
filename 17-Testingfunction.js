64280baa7d31f867c3ab7db2225269543a0fefc2
Santhanamariammal24 <santhanamariammalrajamani1998@gmail.com>
2026-09-22 23:17:10 +0530

    Testing function

---
+++ 17-Testingfunction.js
@@ -0,0 +1,8 @@
+function checkTestStatus(status){
+    if(status == "pass"){
+        console.log("Test Passed");
+    }else{
+        console.log("Test Failed");
+    }
+}
+checkTestStatus("pass"); //calling the function with parameter
